59cb27e030b96c70f33df5d4117145546018b4ca
dagraac <dagraac@naver.com>
2022-09-08 20:36:06 +0900

    work

--- XPlatform/XPlatform.cpp
+++ XPlatform/XPlatform.cpp
@@ -1,8 +1,27 @@
 #include "XPlatform.h"
 
+#include "C:\\Users\\dagraac\\AppData\\Local\\Android\\Sdk\\ndk\\21.4.7075529\\sources\\android\\"
 
 namespace XPlatform
 {
+#if defined(XPLATFORM_WINDOWS)
+#elif defined(XPLATFORM_ANDROID)
+#elif defined(XPLATFORM_IOS)
+#else
+#endif
+
+#if defined(XPLATFORM_WINDOWS)
+#elif defined(XPLATFORM_ANDROID)
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+	void android_main(struct android_app* state)
+	{
+
+	}
+#elif defined(XPLATFORM_IOS)
+#else
+#endif
+
 	void XSound::Clear()
 	{
 	}
