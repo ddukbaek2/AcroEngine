66ae8cf35df59366a927691103dbe9f3bf35eacd
dagraac <dagraac@naver.com>
2022-09-19 03:36:14 +0900

    Update AcroTypeTool.py

--- AcroTool/AcroTypeTool.py
+++ AcroTool/AcroTypeTool.py
@@ -13,11 +13,29 @@ class TypeClass:
 		pass
 
 
+class VariableInfo:
+	_Name : str
+	_Type : str
+
+class FunctionInfo:
+	_Name : str
+	_ReturnTypeNam : str # _Type
+	_ParamterTypes : list # VariableInfo
+
+class ClassInfo:
+	_Name: str
+	_Namespace: str
+	_VariableInfos : VariableInfo
+	_FunctionInfos : FunctionInfo
+
 
 #############################################################################
 # @ 메인함수.
 #############################################################################
 def OnMain(arguments : list):
+	# 인자로 받은 경로 안의 모든 헤더파일을 전부 부른 뒤... 기록한다.
+	# 그리고 기록된 데이터를 저장한다.
+	# 
 	pass
 
 
